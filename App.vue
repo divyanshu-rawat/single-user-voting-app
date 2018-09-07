6b0224d87add77b314454ef1508e472a58087aff
Divyanshu Rawat <divyanshu.r46956@gmail.com>
2018-09-08 03:23:34 +0530

    understanding vue.jse.js concepts

--- App.vue
+++ App.vue
@@ -1,22 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/wow.png">
+    <HelloWorld msg="Welcome to Your Vue.js Application"/>
+    <div class="">A custom component! {{name}}</div>
   </div>
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      name : 'divyanshu'
+    }
   }
 }
+
+
+
 </script>
 
 <style>
+
+* { color: inherit; }
+
+input { font: inherit; border: unset; background: unset; }
+input:focus { outline: unset; }
+
+:root {
+  font: 1rem/1.175 "BlinkMacSystemFont", -apple-system, "Roboto", sans-seri;
+}
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +47,13 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+input { text-align: center; }
+
+img {
+  width: 8rem; height: 8rem;
+  vertical-align: calc(-0.12109375em);
+}
+
 </style>
